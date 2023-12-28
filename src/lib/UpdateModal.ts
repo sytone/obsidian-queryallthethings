b429a56549de03d2c3aecb42b0d7886430e05248
sytone <github@sytone.com>
2023-12-27 16:05:48 -0800

    chore: prepare for next release

--- src/lib/UpdateModal.ts
+++ src/lib/UpdateModal.ts
@@ -70,6 +70,7 @@ export class UpdateModal extends Modal {
 
 Thank you for using Query All the Things! I hope you're enjoying it.
 I'm always looking for ways to improve the plugin, so if you have any feedback, please let me know by creating an <a href="https://github.com/sytone/obsidian-queryallthethings/issues">issue</a>.
+You can download the documentation and example vault to open in obsidian to see the queries in action <A href="https://github.com/sytone/obsidian-queryallthethings/releases/latest/download/obsidian-queryallthethings.vault.zip">here</a>.
 
 And now, here is everything new in Query All the Things since your last update (v${this.previousVersion}).
 
