cb3067145ac67ed1480cc8972ff87781c771e71b
sytone <github@sytone.com>
2024-12-06 13:05:04 -0800

    fix: update post render to use MarkdownPreviewView.render

--- src/PostRender/ObsidianPostRenderer.ts
+++ src/PostRender/ObsidianPostRenderer.ts
@@ -1,9 +1,14 @@
 import {type IPostRenderer} from 'PostRender/IPostRenderer';
-import {MarkdownPreviewView, type Component} from 'obsidian';
+import {type App, MarkdownPreviewView, type Component} from 'obsidian';
 
 export class ObsidianPostRenderer implements IPostRenderer {
+  /**
+   * @param app - The Obsidian application instance.
+   */
+  constructor(private readonly app: App) {}
+
   public async renderMarkdown(renderResults: string, element: HTMLElement, sourcePath: string, component: Component) {
-    await MarkdownPreviewView.renderMarkdown(renderResults, element, sourcePath, component);
+    await MarkdownPreviewView.render(this.app, renderResults, element, sourcePath, component);
     return element.innerHTML;
   }
 }
